dddd3272f7ca0f16215a7cfc068dcb30a6f1a53f
Akash <you@example.com>
2022-11-28 18:58:47 +0530

    Expense Tracker version-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ExpenseForm from './components/ExpenseForm';
+import ExpenseData from './components/ExpenseData';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()
+{
+    let [expenses,updateExpenses]=useState([{title:"College Fees",price:"$288"}]);
+    function getExpenses(expensesdata)
+    {
+       updateExpenses([...expenses,...expensesdata])
+    }
+
+    return(
+        <>
+            <h1 style={{textAlign:'center'}}>EXPENSE TRACKER</h1>
+            <ExpenseForm getdata={getExpenses}/>
+            <ExpenseData expenseData={expenses}/>
+        </>
+    )
 }
 
 export default App;
